2882e223b9d25bc7ce47bf9e65e26aaf721c354a
Zheng Yan <yzheng624@gmail.com>
2012-12-15 10:58:40 +0800

    localhost for mysql.

--- yanshen/settings.py
+++ yanshen/settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': 'yanshen',
-        'HOST': '42.121.18.11',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
