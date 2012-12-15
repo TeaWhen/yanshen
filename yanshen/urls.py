6604c38d40652b2e2feaddcc3111b46fbd924160
Zheng Yan <yzheng624@gmail.com>
2012-12-15 15:27:47 +0800

    ////

--- yanshen/urls.py
+++ yanshen/urls.py
@@ -9,12 +9,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^welcome$', 'yanshen.views.welcome'),
+    url(r'^welcome/$', 'yanshen.views.welcome'),
     url(r'^$', 'yanshen.views.index'),
-    url(r'^me$', 'yanshen.views.me'),
-    url(r'^contact/(\d+)$', 'yanshen.views.contact'),
-    url(r'^group$', 'yanshen.views.group'),
-    url(r'^map$', 'yanshen.views.map'),
+    url(r'^me/$', 'yanshen.views.me'),
+    url(r'^contact/(\d+)/$', 'yanshen.views.contact'),
+    url(r'^group/$', 'yanshen.views.group'),
+    url(r'^map/$', 'yanshen.views.map'),
     url(r'^robots.txt$', 'yanshen.views.robots'),
     url(r'^humans.txt$', 'yanshen.views.humans'),
     # url(r'^yanshen/', include('yanshen.foo.urls')),
