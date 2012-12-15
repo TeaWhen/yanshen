9161ba867d7347a747fdf3658728e6d4e20a531f
Zheng Yan <yzheng624@gmail.com>
2012-12-16 00:10:59 +0800

    baidu map.

--- yanshen/views.py
+++ yanshen/views.py
@@ -21,133 +21,146 @@ import json
 
 @render_to('welcome.html')
 def welcome(request):
-	if request.user.is_authenticated():
-		return redirect('/')
-	appname = "延伸"
-	pagename ='welcome'
-	if request.method == 'POST':
-		username = request.POST['username']
-		password = request.POST['password']
-		action = request.POST['action']
-		if action == 'login':
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				if user.is_active:
-					login(request, user)
-					return redirect('/')
-				else:
-					message = '账户已被注销。'
-					return locals()
-			else:
-				message = 'Email 或密码错误。'
-				return locals()
-		elif action == 'reg':
-			if Profile.objects.filter(email=username).exists():
-			    message = '您已经注册过了。'
-			    return locals()
-			else:
-				try:
-					user = Profile.objects.create_user(email=username, password=password)
-					user.contact_info = json.JSONEncoder().encode({"next_id":2, "data":[{"info_id":1, "type":"Email", "key": u"电子邮箱", "value": user.email}]})
-					user.save()
-				except ValidationError:
-					message = '请输入正确的 Email 地址。'
-					return locals()
-				if user:
-					user = authenticate(username=username, password=password)
-					login(request, user)
-					return redirect('/me/?first=1')
-				else:
-					message = '抱歉，服务器开小差了，注册失败。'
-					return locals()
-	else:
-		return locals()
+    if request.user.is_authenticated():
+        return redirect('/')
+    appname = "延伸"
+    pagename ='welcome'
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        action = request.POST['action']
+        if action == 'login':
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('/')
+                else:
+                    message = '账户已被注销。'
+                    return locals()
+            else:
+                message = 'Email 或密码错误。'
+                return locals()
+        elif action == 'reg':
+            if Profile.objects.filter(email=username).exists():
+                message = '您已经注册过了。'
+                return locals()
+            else:
+                try:
+                    user = Profile.objects.create_user(email=username, password=password)
+                    user.contact_info = json.JSONEncoder().encode({"next_id":2, "data":[{"info_id":1, "type":"Email", "key": u"电子邮箱", "value": user.email}]})
+                    user.save()
+                except ValidationError:
+                    message = '请输入正确的 Email 地址。'
+                    return locals()
+                if user:
+                    user = authenticate(username=username, password=password)
+                    login(request, user)
+                    return redirect('/me/?first=1')
+                else:
+                    message = '抱歉，服务器开小差了，注册失败。'
+                    return locals()
+    else:
+        return locals()
 
 @render_to('index.html')
 @login_required(login_url='/welcome/')
 def index(request):
-	appname = "延伸"
-	pagename = 'index'
-	users = Profile.objects.all()
-	return locals()
+    appname = "延伸"
+    pagename = 'index'
+    users = Profile.objects.all()
+    return locals()
 
 @render_to('contact.html')
 @login_required(login_url='/welcome/')
 def contact(request, pk):
-	appname = "延伸"
-	pagename = 'contact'
-	user = Profile.objects.get(pk=pk)
-	socials = user.conns.all()
-	contact_info = json.JSONDecoder().decode(user.contact_info)['data']
+    appname = "延伸"
+    pagename = 'contact'
+    user = Profile.objects.get(pk=pk)
+    socials = user.conns.all()
+    contact_info = json.JSONDecoder().decode(user.contact_info)['data']
 
-	return locals()
+    return locals()
 
 @render_to('me.html')
 @login_required(login_url='/welcome/')
 def me(request):
-	appname = "延伸"
-	pagename = 'me'
-	user = request.user
-	if request.method == 'POST':
-		action = request.POST['action']
-		if action == 'add':
-			key = request.POST['key']
-			if key == '':
-				key = request.POST['key_custom']
-			value = request.POST['value']
-			type = request.POST['type']
-			contact_info = json.JSONDecoder().decode(user.contact_info)
-			info_id = contact_info['next_id']
-			contact_info['next_id'] += 1
-			contact_info['data'].append(dict(info_id=info_id, key=key, value=value, type=type))
-			user.contact_info = json.JSONEncoder().encode(contact_info)
-			user.save()
-			cats = user.cats.all()
-			for c in cats:
-				cp = json.JSONDecoder().decode(c.privilege)
-				cp[str(info_id)] = False
-				c.privilege = json.JSONEncoder().encode(cp)
-				c.save()
-		else:
-			pass
-	socials = user.conns.all()
-	contact_info = json.JSONDecoder().decode(user.contact_info)['data']
-		
-	return locals()
+    appname = "延伸"
+    pagename = 'me'
+    user = request.user
+    if request.method == 'POST':
+        action = request.POST['action']
+        if action == 'add':
+            key = request.POST['key']
+            if key == '':
+                key = request.POST['key_custom']
+            value = request.POST['value']
+            type = request.POST['type']
+            contact_info = json.JSONDecoder().decode(user.contact_info)
+            info_id = contact_info['next_id']
+            contact_info['next_id'] += 1
+            contact_info['data'].append(dict(info_id=info_id, key=key, value=value, type=type))
+            user.contact_info = json.JSONEncoder().encode(contact_info)
+            user.save()
+            cats = user.cats.all()
+            for c in cats:
+                cp = json.JSONDecoder().decode(c.privilege)
+                cp[str(info_id)] = False
+                c.privilege = json.JSONEncoder().encode(cp)
+                c.save()
+        elif action == 'delete':
+            info_id = int(request.POST['info_id'])
+            contact_info = json.JSONDecoder().decode(user.contact_info)
+            for x in contact_info['data']:
+                if x['info_id'] == info_id:
+                    contact_info['data'].remove(x)
+                    break
+            user.contact_info = json.JSONEncoder().encode(contact_info)
+            user.save()
+            cats = user.cats.all()
+            for c in cats:
+              cp = json.JSONDecoder().decode(c.privilege)
+              cp.pop(str(info_id))
+              c.privilege = json.JSONEncoder().encode(cp)
+              c.save()
+    socials = user.conns.all()
+    contact_info = json.JSONDecoder().decode(user.contact_info)['data']
+        
+    return locals()
 
 @render_to('group.html')
 @login_required(login_url='/welcome/')
 def group(request):
-	appname = "延伸"
-	pagename = 'group'
-	user = request.user
-	categories = Category.objects.filter(owner=user)
-	data = []
-	for category in categories:
-		data.append(dict(category=category, friends=Relationship.objects.filter(from_id=user.id,cat_id=category)))
-	#user.
-	return locals()
+    appname = "延伸"
+    pagename = 'group'
+    user = request.user
+    categories = Category.objects.filter(owner=user)
+    data = []
+    for category in categories:
+        data.append(dict(category=category, friends=Relationship.objects.filter(from_id=user.id,cat_id=category)))
+    #user.
+    return locals()
 
 @render_to('map.html')
 @login_required(login_url='/welcome/')
 def map(request):
-	appname = "延伸"
-	pagename = 'map'
-	return locals()
+    appname = "延伸"
+    pagename = 'map'
+    return locals()
 
 @login_required(login_url='/welcome/')
 def signout(request):
-	logout(request)
-	return redirect('/welcome/')
+    logout(request)
+    return redirect('/welcome/')
 
 def page_not_found(request):
-	return render(request, '404.html')
+    return render(request, '404.html')
 
 def robots(request):
-	return render(request, 'robots.txt', content_type="text/plain")
+    return render(request, 'robots.txt', content_type="text/plain")
 
 def humans(request):
-	return render(request, 'humans.txt', content_type="text/plain")
+    return render(request, 'humans.txt', content_type="text/plain")
 
 def orca(request):
-	return HttpResponse("b5da7f1e4f9066c4", content_type="text/plain")
+    return HttpResponse("b5da7f1e4f9066c4", content_type="text/plain")
