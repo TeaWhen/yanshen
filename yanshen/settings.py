87168a84c5d4191b1eafc65a39bb45ac58348604
Zheng Yan <yzheng624@gmail.com>
2012-12-15 13:57:13 +0800

    mysql setting.

--- yanshen/settings.py
+++ yanshen/settings.py
@@ -16,10 +16,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'yanshen',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'yanshen',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': 'yanshen',
+        'PASSWORD': 'Rpfhn7KbB3w8PVBm',
+        'HOST': '42.121.18.11',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '3306',                      # Set to empty string for default.
     }
 }
 
