cfa8111dd894095e1afa2e9f2b341021013d6985
Zheng Yan <yzheng624@gmail.com>
2012-12-15 11:02:45 +0800

    serving media files.

--- yanshen/urls.py
+++ yanshen/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -22,3 +24,4 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
