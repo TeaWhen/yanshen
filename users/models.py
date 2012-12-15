af32f2d0fc86f3e7450f8be585d96184c4678de2
Zheng Yan <yzheng624@gmail.com>
2012-12-15 14:31:53 +0800

    first_name and last_name.

--- users/models.py
+++ users/models.py
@@ -30,10 +30,13 @@ class Profile(AbstractBaseUser):
     objects = ProfileManager()
 
     email = models.EmailField(verbose_name='email address', max_length=255, unique=True, db_index=True)
+    first_name = models.CharField('first name', max_length=30, blank=True)
+    last_name = models.CharField('last name', max_length=30, blank=True)
+
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
-    contact_info = models.TextField()
+    contact_info = models.TextField(null=True)
     joined = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField()
 
