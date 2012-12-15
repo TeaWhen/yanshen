f773c68952087512a7cd09899624f020737037de
root <root@yanshen.(none)>
2012-12-15 10:31:51 +0800

    add ORCA

--- yanshen/urls.py
+++ yanshen/urls.py
@@ -14,4 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # for orca
+    url(r'^orca.txt$', 'yanshen.views.orca'),
 )
