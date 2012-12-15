d493d259996975be7c722afc2aa3f2bb73cbbb4b
Zheng Yan <yzheng624@gmail.com>
2012-12-15 15:04:53 +0800

    foreach users.

--- users/models.py
+++ users/models.py
@@ -56,7 +56,7 @@ class Profile(AbstractBaseUser):
         """
         Returns the first_name plus the last_name, with a space in between.
         """
-        full_name = '%s %s' % (self.first_name, self.last_name)
+        full_name = '%s %s' % (self.last_name, self.first_name)
         return full_name.strip()
 
     def get_short_name(self):
--- yanshen/views.py
+++ yanshen/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import render_to_response, RequestContext
+from users.models import Profile
 import time
 
 def index(request):
+	users = Profile.objects.all()
 	return render_to_response('index.html', locals(), context_instance=RequestContext(request))
 
 def contact(request):
